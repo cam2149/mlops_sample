a946b4e5e9d7e08c493a472059eeecab425ba9a7
cam2149 <cam2149@gmail.com>
2025-05-01 22:03:11 -0500

    app

--- model/app/app.py
+++ model/app/app.py
@@ -37,4 +37,4 @@ async def procesar_diagnostico():
     return {'diagnostico': prediction[0]}
 
 if __name__ == "__main__":
-    uvicorn.run("app:app",host="127.0.0.1",port=5500, reload=True)
+    uvicorn.run("app:app",host="0.0.0.0",port=5500, reload=True)
